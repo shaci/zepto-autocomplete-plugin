bf7769f866666619554aba96daf44756a92a8b6e
Ivan <mail.igolubev@gmail.com>
2013-01-05 03:09:16 +0400

    storage
    создан див хранлище вариантов выбора, пока что просто базовая версия, он
    появляется по клику на инпуте, и никуда не исчезает

--- zepto-autocomplete.js
+++ zepto-autocomplete.js
@@ -7,20 +7,28 @@
   			height: 40
   		}, options);
 
-
-  		var $this = $(this), data = $this.data('autocomplete'), storage = $('div');
-
+  		var $this = this, data = $this.data('autocomplete');
+  		//если не проинициализирован для данного элемента
   		if (!data) {
-
+  	      var storage = $('<div>');
           $this.data('autocomplete', {
             target:  $this,
             storage: storage
           })
-
-  		  return this.each(function() {
-            $(this).val('tatata1');
-
+  		  return $this.each(function() {
+            $this.bind('click.autocomplete', createStorage)
           })
+  		}
+  		//создание дива с подсказками
+  		function createStorage() {
+  		  var position = { left : $this.offset().left , top : $this.offset().top + $this.height() }
+  		  storage.width($this.width());
+  		  storage.css('position', 'absolute').css('top', position.top).css('left', position.left);
+  		  for (var i = 0; i < settings.data.length; i++) {
+  		  	var record = $('<div>').html(settings.data[i]);
+  		  	storage.append(record);
+  		  }
+  		  $('body').append(storage)
   		}
   	},
   	destroy: function() {
