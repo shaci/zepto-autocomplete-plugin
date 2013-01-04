6e5800a6c7cb70176913e63a64f30a3cfacdbe4c
Ivan <mail.igolubev@gmail.com>
2013-01-05 01:44:02 +0400

    init/destroy functions (empty)

--- zepto-autocomplete.js
+++ zepto-autocomplete.js
@@ -1,7 +1,31 @@
 (function($){
-  $.fn.autoComplete = function() {
+
+  var methods = {
+  	init:    function(options) {
+  		var settings = $.extend({
+  			data:   [],
+  			height: 40
+  		}, options)
+  		console.log('init')
+  	},
+  	destroy: function() {
+  		console.log('destroy')
+  	}
+  }
+
+  $.fn.autoComplete = function(method) {
+
+  	if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Метод с именем ' +  method + ' не существует для Zepto.autocomplete' );
+    }
+
     return this.each(function() {
       $(this).val('tatata')
     })
   }
+
 })(Zepto)
