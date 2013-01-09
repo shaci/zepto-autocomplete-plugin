9513a8b57a85f7d93299d6048151d88c39231b47
Ivan <mail.igolubev@gmail.com>
2013-01-09 16:50:53 +0400

    new options for plugin
    Новый опции для плагина:
    scrollable -возможность скроллинга
    maxOptions - допустимое максимальное количество вариантов (задает
    максимальную высоту дива с вариантами выбора)

--- zepto-autocomplete.js
+++ zepto-autocomplete.js
@@ -3,7 +3,9 @@
   	init:    function(options) {
   		var settings = $.extend({
   			data:   [],
-  			height: 40
+  			height: 20,
+        maxOptions: 100,
+        scrollable: true
   		}, options);
 
   		var $this = this;
@@ -28,6 +30,18 @@
         storage.css('position', 'absolute').css('top', position.top).css('left', position.left).css('border', '1px solid black').css('background', 'white');
         $this.attr('data-autocomplete', 'true');
         storage.on('mousedown.autocomplete', 'div', chooseRecord);
+        if (settings.scrollable) {
+          storage.css('overflow', 'auto');
+        }
+        else {
+          storage.css('overflow', 'hidden')
+        }
+        if (settings.maxOptions > settings.data.length) {
+          storage.css('max-height', settings.data.length * settings.height + 'px');
+        } else {
+          storage.css('max-height', settings.maxOptions * settings.height + 'px');
+        }
+
         $this[0].externalData = {
           storage:  storage,
           settings: settings
@@ -36,6 +50,7 @@
       }
       function closeStorage() {
         var storage = $this[0].externalData.storage;
+        storage[0].scrollTop = 0;
         storage.hide();
       }
       function chooseRecord(e) {
@@ -48,10 +63,11 @@
         storage.empty();
         storage.hide();
         var value = $this.val();
+
         for (var i = 0; i < settings.data.length; i++) {
           var string = settings.data[i];
           if (string.indexOf(value) != -1 && value) {
-            var record = $('<div>').html(settings.data[i]);
+            var record = $('<div>').html(settings.data[i]).css('height', settings.height + 'px');
             storage.append(record);
           }
         }
@@ -62,26 +78,59 @@
         }
       }
 
+      function moveUp(scroll) {
+        var storage  = $this[0].externalData.storage;
+        if (current.prev().length) {
+          if (scroll) {
+            storage[0].scrollTop -= settings.height;
+          }
+          current.css('background', 'white');
+          current = current.prev();
+          current.css('background', 'grey');
+        }
+      }
+
+      function moveDown(scroll) {
+        var storage  = $this[0].externalData.storage;
+        if (current.next().length) {
+          if (scroll) {
+            storage[0].scrollTop += settings.height;
+          }
+          current.css('background', 'white');
+          current = current.next();
+          current.css('background', 'grey');
+        }
+      }
+
   		//создание дива с подсказками
   		function keyHandler(e) {
-        var storage = $this[0].externalData.storage;
+        var storage  = $this[0].externalData.storage;
+        var settings = $this[0].externalData.settings;
         switch (e.keyCode) {
           case 37:
             break;
           case 39:
             break;
           case 38:
-            if (current.prev().length) {
-              current.css('background', 'white');
-              current = current.prev();
-              current.css('background', 'grey');
+            var prev = current.index() - 1;
+            if (!settings.scrollable) {
+              moveUp();
+            } else {
+              var scroll = false;
+              if (prev < storage[0].scrollTop / settings.height) {scroll = true;}
+              moveUp(scroll);
             }
             break;
           case 40:
-            if (current.next().length) {
-              current.css('background', 'white');
-              current = current.next();
-              current.css('background', 'grey');
+            var next = current.index() + 1;
+            if (!settings.scrollable) {
+              if (next < settings.maxOptions) {
+                moveDown();
+              }
+            } else {
+              var scroll = false;
+              if (next >= settings.maxOptions) {scroll = true;}
+              moveDown(scroll);
             }
             break;
           case 13:
@@ -98,7 +147,6 @@
   		}
   	},
   	destroy: function() {
-  		console.log('destroy');
       return this.each(function() {
         $this = $(this);
         $this.unbind('.autocomplete');
@@ -108,16 +156,14 @@
       })
   	},
     add: function(field) {
-
       function unique(arr) {
         var obj = {};
         for(var i=0; i<arr.length; i++) {
           var str = arr[i];
           obj[str] = true;
         }
-          return Object.keys(obj);
+        return Object.keys(obj);
       }
-
       //если не массив
       if (Object.prototype.toString.call(field) != '[object Array]') {
         //попробуем превратить в массив
@@ -127,7 +173,6 @@
       var newArr = $this[0].externalData.settings.data.concat(field);
       newArr     = unique(newArr);
       $this[0].externalData.settings.data = newArr;
-      console.log($this[0].externalData);
     }
   }
 
